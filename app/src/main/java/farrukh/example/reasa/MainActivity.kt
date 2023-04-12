e66b3adf13cd88b29b795be0aa84e09b7598bfd7
PC671 <farrukhburiev001@gmail.com>
2023-04-12 11:26:59 +0500

    spalsh

---
+++ app/src/main/java/farrukh/example/reasa/MainActivity.kt
@@ -0,0 +1,17 @@
+package farrukh.example.reasa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import farrukh.example.reasa.databinding.ActivityMainBinding
+import farrukh.example.reasa.databinding.FragmentSplashBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().add(R.id.main,SplashFragment()).commit()
+
+    }
+}
