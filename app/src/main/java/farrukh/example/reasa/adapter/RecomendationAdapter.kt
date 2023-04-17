9ae7ed320e775b7847874980f9a1b22fce8811c1
PC670 <farrukhburiev001@gmail.com>
2023-04-17 13:20:56 +0500

    main

--- app/src/main/java/farrukh/example/reasa/adapter/RecomendationAdapter.kt
+++ app/src/main/java/farrukh/example/reasa/adapter/RecomendationAdapter.kt
@@ -10,33 +10,31 @@ import androidx.recyclerview.widget.RecyclerView
 import farrukh.example.reasa.R
 import farrukh.example.reasa.model.Featured
 
-class FeaturedAdapter(var offers:MutableList<Featured>):
-    RecyclerView.Adapter<FeaturedAdapter.MyHolder>()  {
+class RecomendationAdapter(var offers:MutableList<Featured>):
+    RecyclerView.Adapter<RecomendationAdapter.MyHolder>()  {
     class MyHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         var name = itemView.findViewById<TextView>(R.id.name)
         var price = itemView.findViewById<TextView>(R.id.price)
         var img = itemView.findViewById<ImageView>(R.id.home_img)
 
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeaturedAdapter.MyHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecomendationAdapter.MyHolder {
         var myHolder = LayoutInflater.from(parent.context).inflate(R.layout.feature_layout,parent,false)
 
-        return FeaturedAdapter.MyHolder(myHolder)
+        return RecomendationAdapter.MyHolder(myHolder)
     }
 
     override fun getItemCount(): Int {
         return offers.size
     }
 
-    override fun onBindViewHolder(holder: FeaturedAdapter.MyHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecomendationAdapter.MyHolder, position: Int) {
         var cont = offers.get(position)
         holder.name.text = cont.name
         holder.price.text = cont.price.toString()
         holder.img.setImageResource(cont.img)
 
     }
-    interface onEditListner{
-        fun onEdit(position: Int, item: Featured)
-    }
+
 
 }
